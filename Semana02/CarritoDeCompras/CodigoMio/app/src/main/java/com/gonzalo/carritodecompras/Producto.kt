ad83b97795adfe82679f0811a1ace3cc8d4935cf
Gonzalo Davila <davilagonzalo2304@gmail.com>
2026-08-26 10:45:05 -0500

    Agregar modelo y productos

---
+++ Semana02/CarritoDeCompras/CodigoMio/app/src/main/java/com/gonzalo/carritodecompras/Producto.kt
@@ -0,0 +1,6 @@
+package com.gonzalo.carritodecompras
+
+data class Producto(
+    val nombre: String,
+    val precio: Double
+)
---
+++ Semana02/CarritoDeCompras/CodigoMio/app/src/main/java/com/gonzalo/carritodecompras/Productos.kt
@@ -0,0 +1,10 @@
+package com.gonzalo.carritodecompras
+
+val productos = listOf(
+    Producto("Polo", 30.0),
+    Producto("Pantalón", 60.0),
+    Producto("Zapatillas", 120.0),
+    Producto("Gorra", 25.0),
+    Producto("Casaca", 90.0),
+    Producto("Mochila", 70.0)
+)
